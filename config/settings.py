42b7d41c7c0177a6743b509b93df73a1777e163f
彭宏紳 (Thomas Peng) <thomas.peng@apmsinc.com>
2026-06-23 14:25:20 +0800

    Prepare Render free deployment

--- config/settings.py
+++ config/settings.py
@@ -21,16 +21,23 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pl8t1z@nw7ojkqre-^)j9_u9$#@x3#()49(wnbrc!ve%6imw5c'
+SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-local-dev-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "True") == "True"
 
 ALLOWED_HOSTS = os.getenv(
     "ALLOWED_HOSTS",
-    "localhost,127.0.0.1"
+    "localhost,127.0.0.1,.onrender.com"
 ).split(",")
 
+csrf_trusted_origins = os.getenv("CSRF_TRUSTED_ORIGINS", "")
+
+CSRF_TRUSTED_ORIGINS = [
+    origin.strip()
+    for origin in csrf_trusted_origins.split(",")
+    if origin.strip()
+]
 
 # Application definition
 
